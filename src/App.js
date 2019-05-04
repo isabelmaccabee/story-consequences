24542c6967b283d6e08fe52f14a92d11a0f931ae
isabelmaccabee <isabel.maccabee@gmail.com>
2019-05-04 20:42:02 +0100

    add basic routing and start screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
+import { Router } from "@reach/router";
 import StartUpWrapper from "./components/StartUpWrapper";
+import WaitingArea from "./components/WaitingArea";
 
 class App extends Component {
   state = {
-    gameToken: "iExist",
-    playerNum: "iExistToo"
+    gameToken: "iExist"
+    // playerNum: "iExistToo"
   };
+
   render() {
     const { gameToken, playerNum } = this.state;
     return (
       <div>
         <StartUpWrapper gameToken={gameToken} playerNum={playerNum}>
           <div className="App">
-            <h1>Welcome to Story Consequences</h1>
+            <h1>Story Consequences</h1>
+            <Router>
+              <WaitingArea path="/waiting-area" />
+            </Router>
           </div>
         </StartUpWrapper>
       </div>
--- src/components/GameCreate.jsx
+++ src/components/GameCreate.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const GameCreate = () => {
-  return <p>Make new game</p>;
-};
+class GameCreate extends Component {
+  state = {
+    nameInput: ""
+  };
+
+  render() {
+    const { nameInput } = this.state;
+    console.log(this.state);
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="nameInput" />
+          <input
+            onChange={this.handleChange}
+            value={nameInput}
+            id="nameInput"
+          />
+          <input type="number" min="1" max="4" />
+          <button type="submit">Create New Game</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange = e => {
+    this.setState({ nameInput: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+}
 
 export default GameCreate;
