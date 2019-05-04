c4acab08df889c5cc4a3792988edfd063af75604
isabelmaccabee <isabel.maccabee@gmail.com>
2019-05-04 21:01:10 +0100

    add form elements to GameCreate and GameJoin

--- src/components/GameCreate.jsx
+++ src/components/GameCreate.jsx
@@ -2,34 +2,40 @@ import React, { Component } from "react";
 
 class GameCreate extends Component {
   state = {
-    nameInput: ""
+    nameInput: "",
+    numOfPlayers: 2
   };
 
   render() {
     const { nameInput } = this.state;
     console.log(this.state);
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="nameInput" />
-          <input
-            onChange={this.handleChange}
-            value={nameInput}
-            id="nameInput"
-          />
-          <input type="number" min="1" max="4" />
-          <button type="submit">Create New Game</button>
-        </form>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="nameInput" />
+        <input onChange={this.handleChange} value={nameInput} id="nameInput" />
+        <label htmlFor="numOfPlayers">No. of players</label>
+        <input
+          type="number"
+          min="2"
+          max="4"
+          onChange={this.handleChange}
+          id="numOfPlayers"
+        />
+        <button type="submit">Create New Game</button>
+      </form>
     );
   }
 
   handleChange = e => {
-    this.setState({ nameInput: e.target.value });
+    const { id, value } = e.target;
+    this.setState({ [id]: value });
   };
 
   handleSubmit = e => {
     e.preventDefault();
+    // send request to db
+    // get token back and set in App
+    // sent to waiting area while waiting for other players to arrive
   };
 }
 
--- src/components/GameJoin.jsx
+++ src/components/GameJoin.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const GameJoin = () => {
-  return <p>Join existing game</p>;
-};
+class GameJoin extends Component {
+  state = {
+    tokenInput: ""
+  };
+  render() {
+    const { tokenInput } = this.state;
+    console.log(this.state);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="tokenInput" />
+        <input
+          id="tokenInput"
+          onChange={this.handleChange}
+          value={tokenInput}
+          type="text"
+        />
+        <button type="submit">Join game</button>
+      </form>
+    );
+  }
+
+  handleChange = e => {
+    this.setState({ tokenInput: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+}
 
 export default GameJoin;
