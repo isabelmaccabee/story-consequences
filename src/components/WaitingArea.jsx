f6a6480f26802e8ee2e3c4206de1322d9056010d
daviidbartlett <daviidbartlett@gmail.com>
2019-05-06 20:09:09 +0100

    add leave game functionality

--- src/components/WaitingArea.jsx
+++ src/components/WaitingArea.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-
-// to go in api.js
 import firebase from "../firebase";
 
 class WaitingArea extends Component {
@@ -8,16 +6,14 @@ class WaitingArea extends Component {
     numOfPlayers: 1
   };
   render() {
-    const { numOfPlayers } = this.props;
+    const { numOfPlayers, leaveGame, gameToken, userId } = this.props;
     return (
       <div className="main">
         <p>hello welcome to the waiting area</p>
-
         <p>
           number of people in the room :{this.state.numOfPlayers}/{numOfPlayers}
         </p>
-
-        <button>leave game</button>
+        <button onClick={() => leaveGame(gameToken, userId)}>leave game</button>
       </div>
     );
   }
@@ -29,19 +25,5 @@ class WaitingArea extends Component {
     });
   }
 }
-
-// componentDidMount() {
-//   const db = firebase.firestore();
-//   db.collection("test")
-//     .doc("x0NWpoBh0lt71ZgqqdZF")
-//     .onSnapshot((doc) => {
-//       const canvas = doc.data().drawing;
-//       const text = doc.data().text;
-//       console.log(doc.data());
-//       this.setState((prevState) => {
-//         return { ...prevState, canvas, write: !prevState.write, text };
-//       });
-//     });
-// }
 
 export default WaitingArea;
