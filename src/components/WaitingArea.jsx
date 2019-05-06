9b27c1b50b793078247ec278d7de07712d67fc97
daviidbartlett <daviidbartlett@gmail.com>
2019-05-06 18:36:40 +0100

    add static player count

--- src/components/WaitingArea.jsx
+++ src/components/WaitingArea.jsx
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 
 class WaitingArea extends Component {
   render() {
+    const { numOfPlayers } = this.props;
     return (
       <div className="main">
-        <p>hello</p>
+        <p>hello welcome to the waiting area</p>
+
+        <p>
+          number of people in the room :{1}/{numOfPlayers}
+        </p>
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log("mounted");
   }
 }
 
