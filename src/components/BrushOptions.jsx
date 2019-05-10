b5c0572b23980ddccf42c2aa7bb2f4a9f5b4b834
isabelmaccabee <isabel.maccabee@gmail.com>
2019-05-10 11:14:45 +0100

    update brush options

--- src/components/BrushOptions.jsx
+++ src/components/BrushOptions.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 class BrushOptions extends Component {
   render() {
+    const { brushRadius, brushOptions } = this.props;
     return (
       <>
         <label>
@@ -13,14 +14,19 @@ class BrushOptions extends Component {
             min="5"
             max="10"
             onChange={this.handleChange}
+            value={brushRadius}
           />
         </label>
         <label>
           Brush colour:
-          <select id="brushColour" onChange={this.handleChange} value="#000000">
-            <option value="#C0C0C0">Light grey</option>
-            <option value="#808080">Dark grey</option>
+          <select
+            id="brushColour"
+            onChange={this.handleChange}
+            value={brushOptions}
+          >
             <option value="#000000">Black</option>
+            <option value="#808080">Dark grey</option>
+            <option value="#C0C0C0">Light grey</option>
           </select>
         </label>
       </>
